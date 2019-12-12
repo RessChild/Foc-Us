d868a059e4ccf5bb7d65055d057d942623289594
ChanGu54 <cksrn5418@konkuk.ac.kr>
2019-12-12 11:00:16 +0900

    최종

--- camera.py
+++ camera.py
@@ -23,7 +23,6 @@ GPIO.setmode(GPIO.BCM) # BCM 으로 사용
 GPIO.setup(servoPIN, GPIO.OUT) # 모터 조작용 정보핀
 GPIO.setup(ledPIN, GPIO.OUT) # 타이머 대용으로 쓸거므로 out
 
-p = GPIO.PWM(servoPIN, 100) # GPIO 17 als PWM mit 50Hz
 GPIO.PWM(ledPIN, 100) # LED PWM
 # 이게 왜 50부터 시작인지는 좀 봐야할거같음
 # LED 에선 PWM이 밝기값을 조절하는 용도
@@ -34,8 +33,6 @@ GPIO.PWM(ledPIN, 100) # LED PWM
 # 정확히는 현재 각도의 값을 몇으로 잡을 것인가를 정의
 
 angle = 10 # 모터가 사용할 기본 (3~17 중간값)
-p.start(angle) # 모터 초기화
-time.sleep(1)
 # 이걸 기반으로 모터 각을 계산하고 이동여부를 판단
 
 # 원래는 현재 기기가 가리키는 각도의 값을 반환받아 쓰고자 했는데
@@ -77,9 +74,15 @@ class Camera(object):
 
 
     def _thread(self):
-        def servoControl(move): # 모터 컨트롤용 함수
+        global angle
+        global p
+        p = GPIO.PWM(servoPIN, 100) # GPIO 17 als PWM mit 50Hz
+        angle = 10
+        p.start(angle)
+        def servoControl(move): 
             global angle
             global p
+            # 모터 컨트롤용 함수
             # 들어온 정보를 기반으로 모터를 회전
             # 카메라 이동 구간
             canMove = True if angle+move < 17 and angle+move > 3 else False
@@ -88,7 +91,7 @@ class Camera(object):
             # 움직일수있다면 움직이고 아니면 고정
             p.ChangeDutyCycle(angle)
             print(angle)
-            time.sleep(2)
+            #time.sleep(2)
             # 실제로 카메라를 움직이는 구간
             # 입출력 시간을 고려해서 몇번 테스트 해봐야할듯
 
@@ -115,8 +118,8 @@ class Camera(object):
         # 탐색해서 대상이 있다고 예상되는 사각형을 갖고 옴
     
                 if len(rects) > 0: # 탐색한 놈이 1개 이상 있다면
-                    #for (x, y , w, h) in rects: # 각 경우에 대해서
-                    #cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 0, 255), 2) # 사각형을 그림
+                    #for (x, y, w, h) in rects: # 각 경우에 대해서
+                    #    cv2.rectangle(self.frame, (x, y), (x+w, y+h), (0, 0, 255), 2) # 사각형을 그림
 
                     # 각 사각형 정보를 사용하기 위해 배열로 변환
                     rects = np.array([[x, y, x + w, y + h] for (x, y, w, h) in rects])
@@ -129,22 +132,23 @@ class Camera(object):
                     cv2.rectangle(p_arr, (x1, y1), (x1 + w1, y1 + h1), (0, 255, 0),2)
                     # 최종탐색
 
-                    if x1 + (w1//2) > 205: # 피사체 중심이 오른쪽으로 쏠린 경우
-                        print("왼쪽 이동")
-                        succ = True if servoControl(1) else False # 그 반대방향으로 이동
-                        if not succ:
-                            print('왼쪽으로 최대치만큼 이동했습니다') # 초점을 맞추기 위해 더이상 움직일 수 없다면, 종료
-                    elif x1 + (w1//2) < 195: #
+                    if x1 + (w1//2) > 215: # 피사체 중심이 오른쪽으로 쏠린 경우
                         print("오른쪽 이동")
-                        succ = True if servoControl(-1) else False
+                        succ = True if servoControl(-1) else False # 그 방향으로 이동
                         if not succ:
-                            print('오른쪽으로 최대치만큼 이동했습니다') # 마찬가지
+                            print('오른쪽으로 최대치만큼 이동했습니다') # 초점을 맞추기 위해 더이상 움직일 수 없다면, 종료
+                    elif x1 + (w1//2) < 185: #
+                        print("왼쪽 이동")
+                        succ = True if servoControl(1) else False
+                        if not succ:
+                            print('왼쪽으로 최대치만큼 이동했습니다') # 마찬가지
                     else: # 그 외의 경우는 초점 잡기 완료
                         print("초점 잡혔음")
                 #
 
                 stream.seek(0)
                 stream.truncate()
+                #time.sleep(0.7)
 
                 #if time.time() - Camera.last_access > 10:
                 #    break
